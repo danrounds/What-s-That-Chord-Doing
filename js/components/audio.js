4102fca0aa09d697533b09c35461600e0620d847
Dan Rounds <dan.rounds@gmail.com>
2017-05-03 17:58:17 -0400

    Make soundfont load with single GET request (rather than one every iteration)

--- js/components/audio.js
+++ js/components/audio.js
@@ -5,48 +5,18 @@ import Soundfont from 'soundfont-player';
 const ac = new AudioContext();
 
 export class Audio extends React.Component {
-    // componentDidMount() {
-    //     Soundfont.instrument(ac, 'acoustic_grand_piano').then(function (piano) {
-    //         for (let i of this.props.notes)
-    //             piano.play(i).stop(ac.currentTime + 0.68);
-
-    //         piano.schedule(ac.currentTime + 0.7, [
-    //             {time: 0, note: 'F4'},
-    //             {time: 0, note: 'A4'},
-    //             {time: 0, note: 'C5'},
-    //             {time: 0, note: 'F5'}
-    //         ]);
-
-    //         piano.play(['C4', 'E4', 'G4']);
-
-    //         piano.schedule(ac.currentTime, ['C4', 'E4', 'G4', 'C5']).stop(ac.currentTime + 0.5);
-    //         piano.schedule(ac.currentTime + 0.7, ['F4', 'A4', 'C5', 'F5'], {duration: 0.5});
-    //         piano.schedule(ac.currentTime + 1.4, ['G4', 'B4', 'D5', 'F5'], {duration: 0.5});
-
-
-        // });
-
-    //     Soundfont.instrument(ac, 'clarinet').then(function(clarinet) {
-    //         clarinet.play('E4');
-    //     });
-
-    //     Soundfont.instrument(ac, 'clarinet').then(function(clarinet) {
-    //         clarinet.play('G4');
-    //     });
-    // }
-
     constructor(props) {
         super(props);
         this.playSounds = this.playSounds.bind(this);
+        this.instrument = Soundfont.instrument(ac, 'acoustic_grand_piano');
     }
 
     playSounds() {
-        Soundfont.instrument(ac, 'acoustic_grand_piano')
-            .then(piano => {
-                for (let i of this.props.notes) {
-                    piano.play(i).stop(ac.currentTime + 0.68);
-                }
-            });
+        this.instrument.then(piano => {
+            for (let i of this.props.notes) {
+                piano.play(i).stop(ac.currentTime + 0.68);
+            }
+        });
     }
 
     componentWillReceiveProps() {
