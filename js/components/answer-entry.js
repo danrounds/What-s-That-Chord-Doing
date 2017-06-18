23859278ede0c1f92f2ea70d975401e872fda77b
Dan Rounds <dan.rounds@gmail.com>
2017-06-18 14:46:13 -0400

    Migrate AnswerButton into AnswerEntry, so that we can use a single event handler

--- js/components/answer-entry.js
+++ js/components/answer-entry.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import {connect} from 'react-redux';
-
-import AnswerButton from './answer-button';
+import * as actions from  '../actions';
 
 export class AnswerEntry extends React.Component {
     constructor(props) {
         super(props);
+        this.onClick = this.onClick.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+
+        const keys = ['q','w','e','r','t','y','u','i','o','p',
+                      'a','s','d','f','g','h','j','k','l',';'];
+
         this.buttons = props.chordSubset.map((numeral) => {
+            let k = keys.shift();
             return (
-                <AnswerButton key={numeral} guess={numeral} />
+                <button key={numeral} onKeyPress={this.handleKeyPress}
+                        onClick={this.onClick}>
+                  {numeral}
+                </button>
             );
         });
+    }
+
+    onClick(e) {
+        if (!this.props.answeredCorrectly) {
+            if (e.target.innerHTML === this.props.currentChord)
+                this.props.dispatch(actions.markTurnCorrect());
+            else
+                this.props.dispatch(actions.incrementGuessN());
+        }
+    }
+
+    handleKeyPress(e) {
+        console.log('OK');
+        if (e.key === this.props.buttonShortcut)
+            // this.onClick();
+            console.log('OK');
     }
 
     render() {
@@ -23,7 +48,9 @@ export class AnswerEntry extends React.Component {
 }
 
 const mapStateToProps = (state, props) => ({
-    chordSubset: state.chordSubset
+    chordSubset: state.chordSubset,
+    currentChord: state.chord,
+    answeredCorrectly: state.answeredCorrectly
 });
 
 export default connect(mapStateToProps)(AnswerEntry);
