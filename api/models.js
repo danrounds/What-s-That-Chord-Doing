807d2cd6e4c8c8912606c770946704662e98a03a
Dan Rounds <dan.rounds@gmail.com>
2017-08-01 11:20:18 -0400

    Add basically-working backend for tallying scores and retrieving high scores

---
+++ api/models.js
@@ -0,0 +1,74 @@
+// schema and wrapper method(s) for our our journal entries
+
+// const bcrypt = require('bcryptjs'); // leaving this here in case we want to
+// implement user accounts (with accompanying password security)
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const individualScoreSchema = mongoose.Schema(
+    {
+        totalClicks: {
+            type: Number,
+            required: true,
+        },
+        nAnsweredRight: {
+            type: Number,
+            required: true,
+        },
+        nQuestionNumber: {
+            type: Number,
+            required: true,
+        },
+        winRatio: {
+            type: Number,
+            require: true,
+        },
+    }
+);
+
+const userScoreSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        scores: {
+            easyMajor: individualScoreSchema,
+            easyMajorInvs: individualScoreSchema,
+
+            easyMinor: individualScoreSchema,
+            easyMinorInvs: individualScoreSchema,
+
+            intermediateMinor: individualScoreSchema,
+            intermediateMinorInvs: individualScoreSchema,
+
+            hardMajor: individualScoreSchema,
+            hardMajorInvs: individualScoreSchema,
+
+            hardMinor: individualScoreSchema,
+            hardMinorInvs: individualScoreSchema,
+
+            allChords: individualScoreSchema,
+            allChordsInvs: individualScoreSchema,
+
+            type: Object,
+            required: true,
+        }
+    }
+);
+
+// userScoreSchema.methods.apiRepr = function() {
+//     return {
+//         // id: this._id,
+//         name: this.name,
+//         totalClicks: this.totalClicks,
+//         nAnsweredRight: this.nAnsweredRight,
+//         nQuestionNumber: this.nQuestionNumber,
+//     };
+// };
+
+// mongoose automagically pluralizes its model names for you. Thanks, mongoose!
+const UserScore = mongoose.model('UserScore', userScoreSchema);
+
+module.exports = {UserScore};
--- api/routes.js
+++ api/routes.js
@@ -4,21 +4,88 @@ const jsonParser = require('body-parser').json();
 const router = express.Router();
 router.use(jsonParser);
 
-router.get('/names/:name', (req, res) => {
-    console.log(req.params.name);
-    res.json({thing: req.params.name});
+const {UserScore} = require('./models');
+
+router.get('/my-scores/:name', (req, res) => {
+    // endpoint for getting all of a user's scores
+    UserScore
+        .findOne({name: req.params.name})
+        .then(record => {
+            if (record)
+                res.json(record);
+            else
+                res.status(404).send();
+        })
+        .catch(() => res.status(500).send());
 });
 
 
-router.put('/names/:name', (req, res) => {
-    console.log(req.params.name);
-    res.json({thing: req.params.name});
+router.put('/my-scores*', (req, res) => {
+    // Endpoint for updating individual user scores
+    // Intent is to only submit one gameMode of updated scores, at once
+    const gameMode = Object.keys(req.body.scores)[0];
+    const requestScores = req.body.scores[gameMode];
+
+    if (['easyMajor','easyMajorInvs','easyMinor','easyMinorInvs',
+         'intermediateMinor','intermediateMinorInvs','hardMajor',
+         'hardMajorInvs','hardMinor','hardMinorInvs','allChords',
+         'allChordsInvs'].indexOf(gameMode) === -1
+        || !req.body.name) {
+        res.status(400).send();
+    }
+
+    for (let el of ['totalClicks','nAnsweredRight','nQuestionNumber'])
+        if (!(el in requestScores)) {
+            return res.status(400).send();
+        }
+
+    // Add win ratio:
+    Object.assign(requestScores, {
+        winRatio:requestScores.nAnsweredRight / requestScores.nQuestionNumber
+    });
+
+    UserScore.findOne({name: req.body.name})
+        .then(record => {
+            if (record) {
+                const newFields = {
+                    scores: Object.assign(record.scores, req.body.scores)
+                };
+                UserScore
+                    .findOne({name: req.body.name})
+                    .update({ $set: newFields })
+                    .then(record => {
+                        if (record.nModified)
+                            res.status(200).send();
+                        else
+                            res.status(400).send();
+                    });
+            } else {
+                const newScore = new UserScore;
+                newScore.name = req.body.name;
+                newScore.scores = req.body.scores;
+                newScore.save()
+                    .then(record => res.status(201).send())
+                    .catch(err => {
+                        if (err.errors.scores.name === 'ValidatorError')
+                            res.status(400).send();
+                        else
+                            res.status(500).send();
+                    });
+            }
+        })
+        .catch(err => res.status(500).json({error: err}));
 });
 
+router.get('/high-scores/:gameType', (req, res) => {
+    const query = {}, match = { name: 1 }, sort = {};
+    query['scores.'+req.params.gameType] = { $exists: true };
+    match['scores.'+req.params.gameType] = 1;
+    sort['scores.'+req.params.gameType+'.winRatio'] = -1;
 
-router.get('/high-scores', (req, res) => {
-    console.log(req.params.name);
-    res.json({thing: 'HI, HI, HIIIIIIIIIIIIIIII'});
+    UserScore.find(query, match)
+        .sort(sort)
+        .then(results => res.json(results))
+        .catch(() => res.status(500).send());
 });
 
 module.exports = {router};
