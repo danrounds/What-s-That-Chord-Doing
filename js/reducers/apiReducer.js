9e34de12a5d32dfa8cf456e8a483797eacdb4608
Dan Rounds <dan.rounds@gmail.com>
2017-08-11 12:09:45 -0400

    Add nearly- working apiReducer

--- js/reducers/apiReducer.js
+++ js/reducers/apiReducer.js
@@ -4,6 +4,21 @@ const initialState = {
     highScores: null,
     userScores: null,
     error: null,
+    pending: null,
+};
+
+function copyUserScores(obj, newObj={ name: obj.name }) {
+    const newScores = {};
+    for (let key of Object.keys(obj.scores)) {
+        newScores[key] = {};
+        Object.assign(newScores[key], obj.scores[key]);
+    }
+    newObj.scores = newScores;
+    return newObj;
+}
+
+function copyHighScores(oldScores) {
+    oldScores.map();
 }
 
 // Our reducer for the API interaction part of our app
@@ -12,34 +27,111 @@ export default (state=initialState, action) => {
 
     case 'GET_USER_SCORES_PENDING':
         return {
-            userScores: 'pending'
+            highScores: 'PUT SOMETHING ELSE HERE',
+            userScores: copyUserScores(state.userScores),
+            error: false,
+            pending: true,
         };
 
     case 'GET_USER_SCORES_SUCCESS':
         return {
-            userScores: action.scores
+            highScores: 'PUT SOMETHING ELSE HERE',
+            userScores: action.scores,
+            error: false,
+            pending: false,
         };
 
     case 'GET_USER_SCORES_FAILURE':
         return {
-            userScores: {},
-            userScores: action.error
+            highScores: 'PUT SOMETHING ELSE HERE',
+            userScores: copyUserScores(state.userScores),
+            error: action.error,
+            pending: false,
         };
 
+        //////////////////////////////////////////////
+        //////////////////////////////////////////////
+
     case 'UPDATE_USER_SCORES_SUCCESS':
+        return {
+            highScores: 'PUT SOMETHING ELSE HERE',
+            userScores: copyUserScores(state.userScores),
+            error: false,
+            pending: false,
+        };
+
     case 'UPDATE_USER_SCORES_FAILURE':
+        return {
+            highScores: 'PUT SOMETHING ELSE HERE',
+            userScores: copyUserScores(state.userScores),
+            error: action.error,
+            pending: false,
+        };
+
+        //////////////////////////////////////////////
+        //////////////////////////////////////////////
 
     case 'GET_HIGH_SCORES_PENDING':
+        return {
+            highScores: 'PUT SOMETHING ELSE HERE',
+            userScores: copyUserScores(state.userScores),
+            error: false,
+            pending: true,
+        };
+
     case 'GET_HIGH_SCORES_SUCCESS':
+        return {
+            highScores: action.highScores,
+            userScores: copyUserScores(state.userScores),
+            error: false,
+            pending: false,
+        };
+
     case 'GET_HIGH_SCORES_FAILURE':
+        return {
+            highScores: 'PUT SOMETHING ELSE HERE',
+            userScores: copyUserScores(state.userScores),
+            error: action.error,
+            pending: false,
+        };
+
+        //////////////////////////////////////////////
+        //////////////////////////////////////////////
 
     case 'MAKE_USER_ACCOUNT_SUCCESS':
-    case 'MAKE_USER_ACCOUNT_FAILURE':
+        return {
+            highScores: 'PUT SOMETHING ELSE HERE',
+            userScores: copyUserScores(state.userScores),
+            error: false,
+            pending: false,
+        };
 
+    case 'MAKE_USER_ACCOUNT_FAILURE':
+        return {
+            highScores: 'PUT SOMETHING ELSE HERE',
+            userScores: copyUserScores(state.userScores),
+            error: action.error,
+            pending: false,
+        };
+
+        //////////////////////////////////////////////
+        //////////////////////////////////////////////
         
     case 'CHANGE_USER_PASSWORD_SUCCESS':
-    case 'CHANGE_USER_PASSWORD_FAILURE':
+        return {
+            highScores: 'PUT SOMETHING ELSE HERE',
+            userScores: copyUserScores(state.userScores),
+            error: false,
+            pending: false,
+        };
 
+    case 'CHANGE_USER_PASSWORD_FAILURE':
+        return {
+            highScores: 'PUT SOMETHING ELSE HERE',
+            userScores: copyUserScores(state.userScores),
+            error: true,
+            pending: false,
+        };
 
     default:
         return state;
