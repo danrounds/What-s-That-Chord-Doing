a75d70ca6a6aa5026c3dffc98275428a6ce7933f
Dan Rounds <dan.rounds@gmail.com>
2017-08-23 01:45:01 -0400

    Fix cursor and user-select styling

--- js/components/KeyboardShortcutsOnOff.js
+++ js/components/KeyboardShortcutsOnOff.js
@@ -22,7 +22,7 @@ export class KeyboardShortcutsOnOff extends React.Component {
     render() {
         if (this.props.showButton)
             return (
-                <div className="nav-button" onClick={this.onClick}>
+                    <div className="nav-button" onClick={this.onClick} style={{ cursor: 'pointer', userSelect: 'none'}}>
                   {this.props.displayKeyboardShortcuts
                    ? (<p className="nav-text-keybrd">Keyboard <br/>hints <span className="nav-on">ON</span></p>)
                    : (<p className="nav-text-keybrd">Keyboard <br/>hints <span className="nav-off">OFF</span></p>)}
